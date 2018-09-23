ce920d6871907880bbf1816194ba995e8c0f97e4
Rashedul Hasan <pori468@gmail.com>
2018-09-23 22:12:03 +0200

    First Basic Project

---
+++ BlockByBlock/Controllers/DummyController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BlockByBlock.Models;
+
+namespace BlockByBlock.Controllers
+{
+    public class DummyController : Controller
+    {
+
+        ApplicationDbContext _data = new ApplicationDbContext();
+
+        // GET: Dummy
+        public ActionResult Index()
+        {
+            
+            var result = _data.Products.ToList();
+
+            return View(result);
+        }
+    }
+}
---
+++ BlockByBlock/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BlockByBlock.MvcApplication" Language="C#" %>
