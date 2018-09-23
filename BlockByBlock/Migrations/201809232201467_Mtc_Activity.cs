9b65ae43c65ac4f33e7cf8b962d2230eb21ebb58
Md Jashim Uddin <jashimduet@yahoo.com>
2018-09-24 00:03:35 +0200

    Create Mtc_Activity Table

---
+++ BlockByBlock/Migrations/201809232201467_Mtc_Activity.cs
@@ -0,0 +1,29 @@
+namespace BlockByBlock.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Mtc_Activity : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Mtc_Activity",
+                c => new
+                    {
+                        Id_act = c.Int(nullable: false, identity: true),
+                        Zone_act = c.Int(nullable: false),
+                        Count_act = c.Int(nullable: false),
+                        Hours_act = c.String(),
+                        Days_act = c.String(),
+                    })
+                .PrimaryKey(t => t.Id_act);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Mtc_Activity");
+        }
+    }
+}
---
+++ BlockByBlock/Models/Mtc_Activity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BlockByBlock.Models
+{
+    public class Mtc_Activity
+    {
+
+        [Key]
+        public int Id_act { get; set; }
+        public int Zone_act { get; set; }
+        public int Count_act { get; set; }
+        public string Hours_act { get; set; }
+        public string Days_act { get; set; }
+    }
+}
