5a6ca1cc1127e168932c4313a5c1db73107444bd
Rashedul Hasan <pori468@gmail.com>
2018-11-05 21:20:20 +0100

    NIcklas3

--- BlockByBlock/Models/Nicklas_View_Model.cs
+++ BlockByBlock/Models/Nicklas_View_Model.cs
@@ -7,22 +7,12 @@ namespace BlockByBlock.Models
 {
     public class Nicklas_View_Model
     {
-        List<Zone> Related_Zone { get; set; }
-
-        public int Radius { get; set; }
-        public int InnerRadius { get; set; }
-        public int MiddleRadius { get; set; }
-        public int OuterRadius { get; set; }
-
-       
-    }
-
-    public class Zone
-    {
         public double Percent { get; set; }
         public double Distance { get; set; }
-        
+
     }
+
+    
 
 
 }
