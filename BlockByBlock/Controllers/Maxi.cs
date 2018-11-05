90ddb6bc5d97969ec85691fef87188bf1cd17fb6
Rashedul Hasan <pori468@gmail.com>
2018-11-05 22:54:06 +0100

    API !!!!

---
+++ BlockByBlock/Controllers/Maxi.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BlockByBlock.Controllers
+{
+    public class Maxi : ApiController
+    {
+
+        public IEnumerable<string> Hellow()
+        {
+            List<string> Name = new List<string>
+            {
+                "Hasan",
+                "Jashim",
+                "Maxi",
+                "Mario"
+            };
+
+
+            return Name;
+        }
+
+
+        //// GET api/<controller>
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        //// GET api/<controller>/5
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        //// POST api/<controller>
+        //public void Post([FromBody]string value)
+        //{
+        //}
+
+        //// PUT api/<controller>/5
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
+
+        //// DELETE api/<controller>/5
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
