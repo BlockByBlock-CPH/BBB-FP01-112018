c9d89c5a8c8f79aa9527b88af9104f61bd41a444
Rashedul Hasan <pori468@gmail.com>
2018-11-14 03:44:31 +0100

    query17

--- BlockByBlock/Controllers/DummyController.cs
+++ BlockByBlock/Controllers/DummyController.cs
@@ -16,6 +16,58 @@ namespace BlockByBlock.Controllers
 
         ApplicationDbContext _data = new ApplicationDbContext();
 
+        public ActionResult Maxi_queri()
+        {
+
+            
+
+            //var players = new List<Player>
+            //{ new Player { Name = "Alex", Team = "A", Score = 10 },
+            //    new Player { Name = "Anna", Team = "A", Score = 20 },
+            //    new Player { Name = "Luke", Team = "L", Score = 60 },
+            //    new Player { Name = "Lucy", Team = "L", Score = 40 }, };
+
+            //var players = new List<Player> { new Player { Name = "Alex", Team = "A", Score = 10 }, new Player { Name = "Anna", Team = "A", Score = 20 }, new Player { Name = "Luke", Team = "L", Score = 60 }, new Player { Name = "Lucy", Team = "L", Score = 40 }, }; var teamTotalScores = from player in players group player by player.Team into playerGroup select new { Team = playerGroup.Key, TotalScore = playerGroup.Sum(x => x.Score), };
+
+            var activity2 = from activity in _data.Mtc_Activities
+                                  group activity by activity.Zone_act into Zone_actGroup
+                                  select new {
+                                      Zone_act = Zone_actGroup.Key,
+                                      People = Zone_actGroup.Sum(x => x.Count_act), };
+
+           
+           // var activity3 = activity2.OrderBy(total => total.People).Take(5).ToList();
+
+            var activity4 = activity2.OrderByDescending(total => total.People).Take(5).ToList();
+
+            List<Query_17_ViewModel> Result = new List<Query_17_ViewModel>();
+
+            
+            foreach (var j in activity4)
+            {
+                var mtc = _data.Mtcs.FirstOrDefault(b => b.Gid == j.Zone_act);
+                Query_17_ViewModel single = new Query_17_ViewModel
+                {
+                    Gid = mtc.Gid,
+                    Id = mtc.Id,
+                    Zone_act = j.Zone_act,
+                    Count_act = j.People,
+                    Geom = mtc.Geom
+
+                };
+
+                Result.Add(single);
+
+
+
+
+            }
+            string sJSONResponse = JsonConvert.SerializeObject(Result);
+
+            return View();
+        }
+
+
         public ActionResult Nicklas()
         
         {
@@ -189,181 +241,181 @@ namespace BlockByBlock.Controllers
 
 
 
-        //public ActionResult Mtc_Activity()
-        //{
-        //    List<Mtc_Activity> Data = new List<Mtc_Activity>();
+        public ActionResult Mtc_Activity()
+        {
+            List<Mtc_Activity> Data = new List<Mtc_Activity>();
 
-        //    var reader = new StreamReader(@"D:\Repositories\BlockByBlock-Resources\Data_Source_from_Tables\Phase1\mtc_activity.csv");
-        //    int i = 0;
-        //    while (!reader.EndOfStream)
-        //    {
-        //        var line = reader.ReadLine();
-        //        var values = line.Split(',');
+            var reader = new StreamReader(@"D:\Repositories\BlockByBlock-Resources\Data_Source_from_Tables\Phase1\mtc_activity.csv");
+            int i = 0;
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var values = line.Split(',');
 
-        //        if (i != 0)
-        //        {
+                if (i != 0)
+                {
 
-        //            values[1] = values[1].Replace("\"", string.Empty);
-        //            values[2] = values[2].Replace("\"", string.Empty);
-        //            values[5] = values[5].Replace("\"", string.Empty);
-        //            values[5] = values[5].Replace(".", ",");
-                   
-        //            Mtc_Activity single = new Mtc_Activity
-        //            {
-        //                Id_act = Int32.Parse(values[0]),
-        //                Zone_act = Int32.Parse(values[1]),
-        //                Count_act = Int32.Parse(values[2]),
-        //                Hours_act = Int32.Parse(values[3]),
-        //                Day_act = Int32.Parse(values[4]),
-        //                Density = Convert.ToDouble(values[5])
+                    values[1] = values[1].Replace("\"", string.Empty);
+                    values[2] = values[2].Replace("\"", string.Empty);
+                    values[5] = values[5].Replace("\"", string.Empty);
+                    values[5] = values[5].Replace(".", ",");
 
-
-        //            };
-
-                    
-        //            Data.Add(single);
-
-        //        }
-        //        i++;
-        //    }
+                    Mtc_Activity single = new Mtc_Activity
+                    {
+                        Id_act = Int32.Parse(values[0]),
+                        Zone_act = Int32.Parse(values[1]),
+                        Count_act = Int32.Parse(values[2]),
+                        Hours_act = Int32.Parse(values[3]),
+                        Day_act = Int32.Parse(values[4]),
+                        Density = Convert.ToDouble(values[5])
 
 
+                    };
 
 
-        //    _data.Mtc_Activities.AddRange(Data);
-        //    _data.SaveChanges();
+                    Data.Add(single);
 
-        //    return View(Data);
-        //}
-
-
-        //public ActionResult Mtc_Homezone()
-        //{
-        //    List<Mtc_Homezone> Data = new List<Mtc_Homezone>();
-
-        //    var reader = new StreamReader(@"D:\Repositories\BlockByBlock-Resources\Data_Source_from_Tables\Phase1\mtc_homezone.csv");
-
-        //    int i = 0;
-        //    while (!reader.EndOfStream)
-        //    {
-        //        var line = reader.ReadLine();
-        //        var values = line.Split(',');
-
-        //        if (i != 0)
-        //        {
-
-        //            values[1] = values[1].Replace("\"", string.Empty);
-        //            values[2] = values[2].Replace("\"", string.Empty);
-        //            values[3] = values[3].Replace("\"", string.Empty);
-        //            values[4] = values[4].Replace("\"", string.Empty);
-        //            values[4] = values[4].Replace(".", ",");
-
-        //            Mtc_Homezone single = new Mtc_Homezone
-        //            {
-        //                Id_hz = Int32.Parse(values[0]),
-        //                Zone_hz = Int32.Parse(values[1]),
-        //                Home_hz = Int32.Parse(values[2]),
-        //                Day_hz = Int32.Parse(values[3]),
-        //                Shared_hz = Convert.ToDouble(values[4])
-        //            };
-                     
-
-        //Data.Add(single);
-
-        //        }
-        //        i++;
-        //    }
+                }
+                i++;
+            }
 
 
 
 
-        //    _data.Mtc_Homezones.AddRange(Data);
-        //    _data.SaveChanges();
+            _data.Mtc_Activities.AddRange(Data);
+            _data.SaveChanges();
 
-        //    return View(Data);
-        //}
-
-        //public ActionResult Mtc()
-        //{
-        //    List<Mtc> Data = new List<Mtc>();
-
-        //    var reader = new StreamReader(@"D:\Repositories\BlockByBlock-Resources\Data_Source_from_Tables\Phase1\mtc(csv)");
+            return View(Data);
+        }
 
 
-        //    int i = 0;
-        //    while (!reader.EndOfStream)
-        //    {
-        //        var line = reader.ReadLine();
-        //        var values = line.Split(';');
-                
+        public ActionResult Mtc_Homezone()
+        {
+            List<Mtc_Homezone> Data = new List<Mtc_Homezone>();
 
-        //        if (i != 0)
-        //        {
+            var reader = new StreamReader(@"D:\Repositories\BlockByBlock-Resources\Data_Source_from_Tables\Phase1\mtc_homezone.csv");
 
-        //            values[2] = values[2].Replace(".", ",");
-        //            values[4] = values[4].Replace(".", ",");
+            int i = 0;
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var values = line.Split(',');
 
-        //            values[4] = values[4].Replace("\"", string.Empty);
-        //            values[3] = values[3].Replace("\"", string.Empty);
+                if (i != 0)
+                {
 
-        //            Mtc single = new Mtc
-        //            {
-        //                Gid = Int32.Parse(values[0]),
-        //                Id = Int32.Parse(values[1]),
-        //                Groesse = Convert.ToDouble(values[2]),
-        //                Geom = values[3],
-        //                Area = Convert.ToDouble(values[4])
+                    values[1] = values[1].Replace("\"", string.Empty);
+                    values[2] = values[2].Replace("\"", string.Empty);
+                    values[3] = values[3].Replace("\"", string.Empty);
+                    values[4] = values[4].Replace("\"", string.Empty);
+                    values[4] = values[4].Replace(".", ",");
 
-        //            };
+                    Mtc_Homezone single = new Mtc_Homezone
+                    {
+                        Id_hz = Int32.Parse(values[0]),
+                        Zone_hz = Int32.Parse(values[1]),
+                        Home_hz = Int32.Parse(values[2]),
+                        Day_hz = Int32.Parse(values[3]),
+                        Shared_hz = Convert.ToDouble(values[4])
+                    };
 
-        //            Data.Add(single);
 
-        //        }
-        //        i++;
-        //    }
+                    Data.Add(single);
 
+                }
+                i++;
+            }
 
 
 
-        //    _data.Mtcs.AddRange(Data);
-        //    _data.SaveChanges();
 
-        //    return View(Data);
-        //}
+            _data.Mtc_Homezones.AddRange(Data);
+            _data.SaveChanges();
 
-        //public ActionResult Mtc_Points()
-        //{
-        //    List<Mtc_Points> Data = new List<Mtc_Points>();
+            return View(Data);
+        }
 
-        //    var reader = new StreamReader(@"D:\Repositories\BlockByBlock-Resources\Data_Source_from_Tables\Phase1\Mtc_Data_Backend.csv");
-        //    int i = 0;
-        //    while (!reader.EndOfStream)
-        //    {
-        //        var line = reader.ReadLine();
-        //        var values = line.Split(',');
+        public ActionResult Mtc()
+        {
+            List<Mtc> Data = new List<Mtc>();
 
-        //        if (i != 0)
-        //        {
-        //            values[1] = values[1].Replace(".", ",");
-        //            values[2] = values[2].Replace(".", ",");
-        //            Mtc_Points single = new Mtc_Points
-        //            {
+            var reader = new StreamReader(@"D:\Repositories\BlockByBlock-Resources\Data_Source_from_Tables\Phase1\mtc(csv)");
 
-        //                MId = Int32.Parse(values[0].Replace("\"", string.Empty)),
-        //                X = Convert.ToDouble(values[1].Replace("\"", string.Empty)),
-        //                Y = Convert.ToDouble(values[2].Replace("\"", string.Empty))
-        //            };
 
-        //            Data.Add(single);
+            int i = 0;
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var values = line.Split(';');
 
-        //        }
-        //        i++;
-        //    }
 
-        //    _data.Mtc_Points.AddRange(Data);
-        //    _data.SaveChanges();
+                if (i != 0)
+                {
 
-        //    return View(Data);
-        //}
+                    values[2] = values[2].Replace(".", ",");
+                    values[4] = values[4].Replace(".", ",");
+
+                    values[4] = values[4].Replace("\"", string.Empty);
+                    values[3] = values[3].Replace("\"", string.Empty);
+
+                    Mtc single = new Mtc
+                    {
+                        Gid = Int32.Parse(values[0]),
+                        Id = Int32.Parse(values[1]),
+                        Groesse = Convert.ToDouble(values[2]),
+                        Geom = values[3],
+                        Area = Convert.ToDouble(values[4])
+
+                    };
+
+                    Data.Add(single);
+
+                }
+                i++;
+            }
+
+
+
+
+            _data.Mtcs.AddRange(Data);
+            _data.SaveChanges();
+
+            return View(Data);
+        }
+
+        public ActionResult Mtc_Points()
+        {
+            List<Mtc_Points> Data = new List<Mtc_Points>();
+
+            var reader = new StreamReader(@"D:\Repositories\BlockByBlock-Resources\Data_Source_from_Tables\Phase1\Mtc_Data_Backend.csv");
+            int i = 0;
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var values = line.Split(',');
+
+                if (i != 0)
+                {
+                    values[1] = values[1].Replace(".", ",");
+                    values[2] = values[2].Replace(".", ",");
+                    Mtc_Points single = new Mtc_Points
+                    {
+
+                        MId = Int32.Parse(values[0].Replace("\"", string.Empty)),
+                        X = Convert.ToDouble(values[1].Replace("\"", string.Empty)),
+                        Y = Convert.ToDouble(values[2].Replace("\"", string.Empty))
+                    };
+
+                    Data.Add(single);
+
+                }
+                i++;
+            }
+
+            _data.Mtc_Points.AddRange(Data);
+            _data.SaveChanges();
+
+            return View(Data);
+        }
     }
 }
---
+++ BlockByBlock/Models/Query_ViewModels.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BlockByBlock.Models
+{
+    public class Query_ViewModels
+    {
+    }
+    public class Query_17_ViewModel
+    {
+        public int Gid { get; set; }
+        public int Id { get; set; }
+        public int Zone_act { get; set; }
+        public int Count_act { get; set; }
+        public string Geom { get; set; }
+
+    }
+}
